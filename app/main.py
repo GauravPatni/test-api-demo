d53b6fb2686dec31c48008922ef59a06bb7a532a
Gaurav <patni.gaurav@jains.com>
2021-01-14 13:22:39 +0530

    init 6

--- app/main.py
+++ app/main.py
@@ -5,19 +5,32 @@ from flask import Flask, render_template, send_from_directory
 from flask_restful import Resource, Api
 from flask import request, Response
 
+
+def getData():
+  resp = requests.get("https://api.wazirx.com/api/v2/tickers")
+  data = resp.json()
+  return data
+
+
+
 class API(Resource):
 
     def get(self):
         print('Get  REQ')
-        return "Test API ", 200
+        return "Test API", 200
 
     def post(self):
         req = request.get_json()
-        print(req)
+        
         if req:
-            return req , 200
+          if "WZ" in req:
+            return getData() , 200
+          return "Bad Request", 403
         else:
             return "Bad Request", 403
+
+
+
 
 
 app = Flask(__name__)
