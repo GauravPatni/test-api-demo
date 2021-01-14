eb4c114fabbacfee6705d26f22b895832d474788
Gaurav <patni.gaurav@jains.com>
2021-01-14 12:53:52 +0530

    init 3

--- app/main.py
+++ app/main.py
@@ -1,12 +1,32 @@
 import time
-
+import requests
 
 from flask import Flask, render_template, send_from_directory
+from flask_restful import Resource, Api
+from flask import request, Response
 
-app= Flask(__name__)
+class API(Resource):
+
+    def get(self):
+        print('Get  REQ')
+        return "Test API ", 200
+
+    def post(self):
+        req = request.get_json()
+        print(req)
+        if req:
+            return req , 200
+        else:
+            return "Bad Request", 403
 
 
-@app.route('/')
-def index():
-  return "<h1>Welcome to CodingX</h1>"
+app = Flask(__name__)
+api = Api(app)
+
+
+api.add_resource(API, '/api')
+
+print("Starting IrriCare-Pro V0.7")
+app.run(host='0.0.0.0', port=3005, debug=False)
+
 
